262761c0c5492129e72f0a7551e3d1f60dcd8111
memlis <memlis@gmail.com>
2017-01-19 17:27:36 +0800

    restructure router

--- api/server.go
+++ api/server.go
@@ -1,70 +1,68 @@
 package api
 
 import (
-	"github.com/Sirupsen/logrus"
-	"github.com/gorilla/mux"
-	"github.com/memlis/boat/api/router"
-	"net"
-	"net/http"
+ 	"github.com/gorilla/mux"
+ 	"net"
+ 	"net/http"
+	"log"
 )
 
 type Server struct {
 	addr    string
-	sock    string
 	routers []router.Router
 }
 
-func NewServer(addr, sock string) *Server {
-	return &Server{
-		addr: addr,
-		sock: sock,
+type handlerFunc func(w http.ResponseWriter, r *http.Request)
+
+func NewServer(addr string) *Server {
+	s := &Server {
+            addr: addr,
 	}
+
+	s.setupRouters()
+
+	return s
 }
 
-// createMux initializes the main router the server uses.
-func (s *Server) createMux() *mux.Router {
+func (s *Server) setupRouters() {
+	s.routers = map[string]map[string]handlerFunc{
+                "GET": map[string]handleFunc{
+                        "/v1/jobs":          getJobs,
+                        "/v1/jobs/:id":          getJob,
+                },  
+                "POST": map[string]handleFunc{
+                        "/jobs":    startJob,
+                        "/jobs/:id":      updateJob,
+                        "/jobs/:id/scale":      scaleJob,
+                },  
+                "DELETE": map[string]handleFunc{
+                        "/jobs/:id":  delJob,
+                },  
+        }   
+}
+func (s *Server) createHandler() *mux.Router {
 	m := mux.NewRouter()
 
-	logrus.Debug("Registering routers")
-	for _, router := range s.routers {
-		for _, r := range router.Routes() {
-			f := s.makeHTTPHandler(r.Handler())
-
-			logrus.Debugf("Registering %s, %s", r.Method(), r.Path())
-			m.Path(r.Path()).Methods(r.Method()).Handler(f)
+	for method, router := range s.routers {
+		for path, handler := range router {
+			m.Path(path).Methods(method).Handler(handler)
 		}
 	}
 
 	return m
 }
 
-func (s *Server) makeHTTPHandler(handler router.APIFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		logrus.WithFields(logrus.Fields{"from": r.RemoteAddr}).Infof("[%s] %s", r.Method, r.URL.Path)
-		if err := handler(w, r); err != nil {
-			logrus.Errorf("Handler for %s %s returned error: %v", r.Method, r.URL.Path, err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	}
-}
-
-// InitRouter initializes the list of routers for the server.
-func (s *Server) InitRouter(routers ...router.Router) {
-	for _, r := range routers {
-		s.routers = append(s.routers, r)
-	}
-}
-
 func (s *Server) ListenAndServe() error {
-	srv := &http.Server{
+ 	srv := &http.Server{
 		Addr:    s.addr,
-		Handler: s.createMux(),
+		Handler: s.createHandler(),
 	}
-	logrus.Infof("API Server listen on %s", s.addr)
+	log.Println("API Server listen on %s", s.addr)
 	ln, err := net.Listen("tcp", s.addr)
 	if err != nil {
-		logrus.Errorf("Listen on %s error: %s", s.addr, err)
+		log.Println("Listen on %s error: %s", s.addr, err)
 		return err
 	}
 	return srv.Serve(ln)
 }
+
